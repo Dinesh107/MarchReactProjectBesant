98c64dc80c90577a625a4994e32bfa8a679ca6e6
Dinesh107 <dineshjr107@gmail.com>
2024-04-02 20:21:07 +0530

    SecondCommitApr2

---
+++ src/Components/back/Data/Data.js
@@ -0,0 +1,62 @@
+const data = {
+
+    productItems: [
+        {
+            id: "1",
+            name: "Headphone",
+            price: 1000,
+            image: "./Pictures/headphone.webp"
+        },
+        {
+            id: "2",
+            name: "laptop",
+            price: 30000,
+            image: "./Pictures/Laptop.jpeg"
+        },
+        {
+            id: "3",
+            name: "watch",
+            price: 1500,
+            image: "./Pictures/watch.jpeg"
+        },
+        {
+            id: "4",
+            name: "iphone",
+            price: 444,
+            image: "./Pictures/iphone.webp"
+        },
+        {
+            id: "5",
+            name: "phone",
+            price: 10000,
+            image: "./Pictures/Phone.jpeg"
+        },
+        {
+            id: "6",
+            name: "Silver Bracelet",
+            price: 1500,
+            image: "./Pictures/bracelets.webp"
+        },
+        {
+            id: "7",
+            name: "Gold Ring",
+            price: 2500,
+            image: "./Pictures/goldring.webp"
+        },
+        {
+            id: "8",
+            name: "BackPack",
+            price: 500,
+            image: "./Pictures/bag.jpeg"
+        },
+        {
+            id: "9",
+            name: "Samsung Tablet",
+            price: 9999,
+            image: "./Pictures/tab.webp"
+        },
+    ]
+
+}
+
+export default data;
